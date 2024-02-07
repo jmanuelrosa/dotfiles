127426b6c719705b80bc355b1266af0d7ea5457c
Jose Manuel Rosa <jose.moncayo@lenus.io>
2024-02-07 14:26:50 +0100

    feat: remove unsed code for github, and add more apps and config

--- roles/shell/files/fish/conf.d/aliases.fish
+++ roles/shell/files/fish/conf.d/aliases.fish
@@ -1,17 +1,24 @@
 # system
 alias rm "rm -i"
+alias cat bat
+alias less bat
 
 # control
 alias please sudo
-alias restart 'sudo shutdown -r now'
-alias shutdown 'sudo shutdown now'
+alias restart "sudo shutdown -r now"
+alias shutdown "sudo shutdown now"
 
 # update
 alias update "sudo reflector --protocol https --age 5 --number 10 --latest 10 --sort rate --save /etc/pacman.d/mirrorlist && yay -Sc --noconfirm && yay -Syu --noconfirm && yay -RYc"
 
-#navigartion
+#navigation
 alias .. "cd .." # Exists by default in fish
-# alias ll "ls -alh" # Exists by default in fish
+alias ll "ls -alh" # Exists by default in fish
+
+# NPM
+alias p pnpm
 
 # apps
 alias lock "i3lock"
+alias g "git"
+alias chrome "open -a google\ chrome"
